1c73079695c7fa83cf6603b16b5cadcc961d6c65
Erick A. Teixeira <erickantunest@gmail.com>
2020-04-01 01:58:57 -0300

    add gui directory for python programs

---
+++ exercicios/python/gui/helloworld.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+# criando uma label com hello world printado
+myLabel = Label(root, text="hello world")
+# colocando a label na tela
+myLabel.pack()
+
+root.mainloop()
